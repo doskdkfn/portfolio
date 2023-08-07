55190965eb5227338702d9a7547c7da56e1e1d3a
doskdkfn <dawn_1216@naver.com>
2023-08-07 17:07:21 +0900

    2023-08-07

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#logo").click(function () {
+        $("#side-box").animate({right: "0"}, 500);
+        // $("#side-box").fadeIn(600);
+        $("#logo").fadeOut(600);
+    });
+
+    $(".close-btn").click(function () {
+        $("#side-box").animate({right: "-20%"}, 500);
+        // $("#side-box").fadeOut(600);
+        $("#logo").fadeIn(600);
+    });
+});;
