65f19eafe6e62674279df0543ea1dfbd3a93eaff
doskdkfn <dawn_1216@naver.com>
2023-08-10 15:53:01 +0900

    2023-08-10

--- js/script.js
+++ js/script.js
@@ -14,7 +14,31 @@ $(document).ready(function () {
 
 });;
 
-// $('html, body').animate({
-//     scrollTop: target.offset().top
-//   }, 1000, function() {
-// });
+
+var $html = $("html");
+ 
+var page = 1;
+ 
+// var lastPage = $("footer").length;
+ 
+$html.animate({scrollTop:0},10);
+
+
+$(window).on("wheel", function(e){
+ 
+	if($html.is(":animated")) return;
+ 
+	if(e.originalEvent.deltaY > 0){
+		if(page== 2) return;
+ 
+		page++;
+	}else if(e.originalEvent.deltaY < 0){
+		if(page == 1) return;
+ 
+		page--;
+	}
+	var posTop = (page-1) * $(window).height();
+ 
+	$html.animate({scrollTop : posTop});
+ 
+});
