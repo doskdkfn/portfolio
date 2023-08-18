aa1969f2310409d6a5895dc5754d806af8b2e95e
doskdkfn <dawn_1216@naver.com>
2023-08-18 17:09:57 +0900

    2023-08-18 donut garph

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   // 사이드 메뉴바 
   $("#logo").click(function () {
-    $("#side-box").animate({ right: "0" }, 500);
+    $("#side-box").animate({ right: "0" }, 600);
     // $("#side-box").fadeIn(600);
     $("#logo").fadeOut(600);
   });
 
   $(".close-btn").click(function () {
-    $("#side-box").animate({ right: "-100%" }, 500);
+    $("#side-box").animate({ right: "-100%" }, 1200);
     // $("#side-box").fadeOut(600);
     $("#logo").fadeIn(600);
   });
@@ -36,3 +36,15 @@ $(window).on("wheel", function(e) {
 
 /////////  도넛 그래프 애니메이션
 
+const donut = document.querySelector(".donut")
+donut.dataset.percent = totalMinwon
+donut.style.background = `conic-gradient(#3F8BC9 0% ${totalMinwon}%, #F2F2F2 ${totalMinwon}% 100%)`
+
+let t5 = 0
+const donutAnimation2 = setInterval(() => {
+  donut2.dataset.percent = t5
+  donut2.style.background = totalMinwon > t5 ? `conic-gradient(#4F98FF 0 ${t3}%, #dedede ${t3}% 100% )` : 
+  totalMinwon + resolveMinwon > t5 ? `conic-gradient(#4F98FF 0 ${totalMinwon}%, #f44336 ${totalMinwon}% ${t3}%, #dedede ${t3}% 100%)` : 
+  `conic-gradient(#4F98FF 0 ${totalMinwon}%, #f44336 ${totalMinwon}% ${totalMinwon + resolveMinwon}%, #dedede ${totalMinwon + resolveMinwon}% 100%)`
+  t5++ >= totalMinwon + resolveMinwon && clearInterval(donutAnimation2)
+}, 10)
