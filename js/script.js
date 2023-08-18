7b219362af886df1471d0ac458dcd4a2e9c8b08b
doskdkfn <dawn_1216@naver.com>
2023-08-18 16:16:49 +0900

    2023-08-18 donut garph

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
 $(document).ready(function () {
-           // 사이드 메뉴바 
-    $("#logo").click(function () {
-        $("#side-box").animate({right: "0"}, 500);
-        // $("#side-box").fadeIn(600);
-        $("#logo").fadeOut(600);
-    });
+  // 사이드 메뉴바 
+  $("#logo").click(function () {
+    $("#side-box").animate({ right: "0" }, 500);
+    // $("#side-box").fadeIn(600);
+    $("#logo").fadeOut(600);
+  });
 
-    $(".close-btn").click(function () {
-        $("#side-box").animate({right: "-100%"}, 500);
-        // $("#side-box").fadeOut(600);
-        $("#logo").fadeIn(600);
-    });
+  $(".close-btn").click(function () {
+    $("#side-box").animate({ right: "-100%" }, 500);
+    // $("#side-box").fadeOut(600);
+    $("#logo").fadeIn(600);
+  });
 
-});;
+});
 
+/////////  마우스 스크롤 시 한 섹션씩 넘어감
 
-const donut = document.querySelector(".donut")
-donut.dataset.percent = totalMinwon
-donut.style.background = `conic-gradient(#3F8BC9 0% ${totalMinwon}%, #F2F2F2 ${totalMinwon}% 100%)`
+var mHtml = $("html");
+var page = 1;
 
-let t4 = 0
-const donutAnimation = setInterval(() => {
-  donut.dataset.percent = t4
-  donut.style.background = `conic-gradient(#4F98FF 0 ${t4}%, #DEDEDE ${t4}% 100% )`
+mHtml .animate({scrollTop : 0}, 10);
 
-  t4++ >= totalMinwon && clearInterval(donutAnimation)
-}, 10)
+$(window).on("wheel", function(e) {
+  if(mHtml.is(":animated")) return;
+  if(e.originalEvent.deltaY > 0) {
+      if(page == 4) return;
+      page++;
+  } else if(e.originalEvent.deltaY < 0) {
+      if(page == 1) return;
+      page--;
+  }
+  var posTop =(page-1) * $(window).height();
+  mHtml.animate({scrollTop : posTop});
+})
 
-let t5 = 0
-const donutAnimation2 = setInterval(() => {
-  donut2.dataset.percent = t5
-  donut2.style.background = totalMinwon > t5 ? `conic-gradient(#4F98FF 0 ${t3}%, #dedede ${t3}% 100% )` : 
-  totalMinwon + resolveMinwon > t5 ? `conic-gradient(#4F98FF 0 ${totalMinwon}%, #f44336 ${totalMinwon}% ${t3}%, #dedede ${t3}% 100%)` : 
-  `conic-gradient(#4F98FF 0 ${totalMinwon}%, #f44336 ${totalMinwon}% ${totalMinwon + resolveMinwon}%, #dedede ${totalMinwon + resolveMinwon}% 100%)`
-  t5++ >= totalMinwon + resolveMinwon && clearInterval(donutAnimation2)
-}, 10)
+/////////  도넛 그래프 애니메이션
+
