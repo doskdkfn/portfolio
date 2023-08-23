e7a0ef0e027649ec3ab3961f1d2dcf3f62915dca
doskdkfn <dawn_1216@naver.com>
2023-08-23 15:22:23 +0900

    2023-08-23 section4 design

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ mHtml .animate({scrollTop : 0}, 10);
 $(window).on("wheel", function(e) {
   if(mHtml.is(":animated")) return;
   if(e.originalEvent.deltaY > 0) {
-      if(page == 4) return;
+      if(page == 5) return;
       page++;
   } else if(e.originalEvent.deltaY < 0) {
       if(page == 1) return;
